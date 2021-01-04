d1774d3f1333b93574479c17b3f483287b8c548b
MohamedSamir <mhmdsamir92@gmail.com>
2021-01-04 13:57:53 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='samir_example',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/mhmdsamir92/packaging',
+    author='Mohamed Samir',
+    author_email='mhmdsamir92@gmail.com',
+    license='BSD 2-clause',
+    packages=['samir_example'],
+    install_requires=[],
+)
