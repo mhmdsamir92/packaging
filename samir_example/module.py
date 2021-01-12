c241d9ff400412b0b1083b7e793f4b11ab3d76a3
MohamedSamir <mhmdsamir92@gmail.com>
2021-01-12 12:37:16 +0200

    Package2

--- samir_example/module.py
+++ samir_example/module.py
@@ -1,4 +1,6 @@
-def add(x, y):
-	return x + y
+from wolfpack.math import add
+
+def samir_add(x, y):
+	return add(x, y)
 if __name__ == "__main__":
-	print(add(7, 8))
+	print(samir_add(7, 8))
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='samir_example',
-    version='0.1.0',    
+    version='0.1.1',    
     description='A example Python package',
     url='https://github.com/mhmdsamir92/packaging',
     author='Mohamed Samir',
     author_email='mhmdsamir92@gmail.com',
     license='BSD 2-clause',
-    packages=['samir_example'],
+    packages=['samir_example', 'wolfpack'],
     install_requires=[],
 )
